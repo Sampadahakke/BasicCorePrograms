69e9b340fdf7cf3cdc18d957a821087ec9fb0ba2
Sampada <sampadahakke102@gmail.com>
2022-02-10 22:30:08 +0530

    Adding Coin Flip Percentage Problem

---
+++ BasicCoreProgram/BasicCoreProgram/BasicCoreProgram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class BasicCoreProgram
+    {
+        public int HeadCount = 0;
+        public int TailCount = 0;
+        public int HeadPercentage = 0;
+        public int TailPercentage = 0;
+        Random random = new Random();
+        public void PercentageCheck()
+        {
+            Console.WriteLine("Enter the number of times you want to flip the coin");
+            int CoinFlip = Convert.ToInt32(Console.ReadLine());
+
+            //Using For Loop For Number Of Times Coin Flip
+            for (int i = 0; i < CoinFlip; i++)
+            {
+                double CheckFlip = random.Next(0, 2);
+                if (CheckFlip < 0.5)
+                {
+                    Console.WriteLine("This is Head");
+                    HeadCount++;
+                }
+                else
+                {
+                    Console.WriteLine("This is Tail");
+                    TailCount++;
+                }
+            }
+            //Calculation of percentage 
+            HeadPercentage = HeadCount * (100 / CoinFlip);
+            TailPercentage = TailCount * (100 / CoinFlip);
+            Console.WriteLine("Percentage of head :" + HeadPercentage);
+            Console.WriteLine("Percentage of head :" + TailPercentage);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================Welcome to Basic Core Program====================");
+            BasicCoreProgram program = new BasicCoreProgram();
+            program.PercentageCheck();
+            Console.ReadLine();
+        }
+    }
+}
